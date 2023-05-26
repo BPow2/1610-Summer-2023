0f434058743207b133e270639884decb157d17b8
BPow2 <133279262+BPow2@users.noreply.github.com>
2023-05-25 18:55:59 -0600

    ClassExample.cs

---
+++ ClassExample.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class ClassExample
+{
+    
+}
+
+public class ClassWithVariables
+{
+    public int number = 2;
+    public string userName = "Bob";
+	public int age = 40;
+	public float weight = 220.3f;
+	public float height = 5.6f;
+}
+
+public class ClassWithFunctions
+{
+    public void RunFunction()
+    {
+        Console.WriteLine("I'm Bob");
+    }
+}   
