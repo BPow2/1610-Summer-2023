4372eb8e79adb9f24d4459bbc2feadfb955e0203
BPow2 <133279262+BPow2@users.noreply.github.com>
2023-05-31 21:19:11 -0600

    Update Unit01b-Lab_GradeCalculator.cs
    Update

--- Unit 01b-BeginnerScripting/Unit01b-Lab_GradeCalculator.cs
+++ Unit 01b-BeginnerScripting/Unit01b-Lab_GradeCalculator.cs
@@ -5,17 +5,21 @@ public class Program
 	public void Main()
 	{
 		Console.WriteLine("What is your exam score? (0-100) ");
+		//asks the user what their score is from 0-100
 		int examScore = Convert.ToInt32(Console.ReadLine());
 		LetterGrade(examScore);
 	}
 	
 	public void LetterGrade (int examScore) {
+		//checks to see if exam score is over 90
 		if (examScore >= 90) {
 			Console.WriteLine("A");	
+			//checks to see if exam score is less than 90 but great than 70
 	} else if (examScore < 90
 			  && examScore > 70) {
 			Console.WriteLine("B");
-		} else if (examScore > 50
+			//checks to see if exam score is greater than 50 but also less than 70
+ 		} else if (examScore > 50
 				  && examScore < 70) {
 			Console.WriteLine("C");
 		}
