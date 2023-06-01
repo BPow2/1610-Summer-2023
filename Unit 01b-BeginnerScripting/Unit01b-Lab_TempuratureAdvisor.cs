7a0776068b591b1a3d27529adf0061238f62d1ab
BPow2 <133279262+BPow2@users.noreply.github.com>
2023-05-31 21:20:10 -0600

    Update Unit01b-Lab_TempuratureAdvisor.cs
    update

--- Unit 01b-BeginnerScripting/Unit01b-Lab_TempuratureAdvisor.cs
+++ Unit 01b-BeginnerScripting/Unit01b-Lab_TempuratureAdvisor.cs
@@ -5,6 +5,7 @@ public class Program
 	
 	public void Main()
 	{
+		//asks user what the current temperature is in celsius
 		Console.WriteLine("What is the current tempurature? (in Celsius)");
 		int currentTemp = Convert.ToInt32(Console.ReadLine());
 		int requiredTemp = 30;
@@ -13,6 +14,7 @@ public class Program
 	}
 	
 	public void Temperature (int currentTemp, int requiredTemp) {
+		//checks to see if the current temp is less than or greater than 30
 	if (currentTemp > requiredTemp) {
 		Console.WriteLine("Don't forget to stay hydrated and avoid being in the sun for extended periods of time.");
 	} else {
