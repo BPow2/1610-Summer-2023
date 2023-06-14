f259d1690b9c39e416500c733f80090719f79959
Bryan Powell <b.pow2.bjp@gmail.com>
2023-06-14 16:41:47 -0600

    Unit 3 - Activity (Update)

---
+++ Unit 3/Game/My project/Assets/ScriptThree.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicSyntax : MonoBehaviour
+{
+    void Start()
+    {
+        //this line is there to tell me the x position of my object
+
+        /* Hi there !
+         * this is two lines!
+         * */
+        Debug.Log("I'm about to hit the ground!");
+    }
+}
