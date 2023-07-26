f3674e5118e6a074b0a008308531f41f7c792ccd
Bryan Powell <b.pow2.bjp@gmail.com>
2023-07-25 21:06:24 -0600

    Lab 7 (WIP)

---
+++ Unit 7/Lab 7/Assets/Scripts/IntData.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class IntData : ScriptableObject
+{
+    public int value;
+
+    public void updateValue(int num)
+    {
+        value += num;
+        Debug.Log(value);
+    }
+}
